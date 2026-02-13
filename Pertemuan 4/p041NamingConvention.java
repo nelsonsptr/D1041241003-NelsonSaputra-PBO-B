d2392dd4b5ccedeb8e362f56bc4988632de19e45
nelsonputra <nelson.putra.id@gmail.com>
2026-02-13 08:54:42 +0700

    Pertemuan 4

---
+++ Pertemuan 4/p041NamingConvention.java
@@ -0,0 +1,20 @@
+public class p041NamingConvention {
+    /*
+    Nama    : Nelson Saputra
+    NIM     : D1041241003
+    Kelas   : PBO B
+    Tanggal : 13 Februari 2026
+    */
+   public static final double PI = 3.14;
+   public static void main(String[] args) {
+    //Variable dengan camelCase
+    int studentAge = 20;
+    String studentName = "Budi";
+    boolean isActive = true;
+
+    System.out.println("Nama            : " + studentName);
+    System.out.println("Umur            : " + studentAge);
+    System.out.println("Status          : " + isActive);
+    System.out.println("Konstanta PI    : " + PI);
+   }
+}
---
+++ Pertemuan 4/p042OperatorAritmatika.java
@@ -0,0 +1,26 @@
+public class p042OperatorAritmatika {
+    /*
+    Nama    : Nelson Saputra
+    NIM     : D1041241003
+    Kelas   : PBO B
+    Tanggal : 13 Februari 2026
+    */
+   public static void main(String[] args) {
+    int a = 10;
+    int b = 3;
+
+    System.out.println("a = " + a);
+    System.out.println("b = " + b);
+    System.out.println();
+
+    System.out.println("a + b = " + (a + b));
+    System.out.println("a - b = " + (a - b));
+    System.out.println("a * b = " + (a * b));
+    System.out.println("a / b = " + (a / b));
+    System.out.println("a % b = " + (a % b));
+
+    //Pembagian dengan desimal
+    double hasil = (double) a / b;
+    System.out.println("a / b (desimal) = " + hasil);
+   }
+}
