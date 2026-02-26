b7809c71e89c382e18f57e1e729d42d7af909a6a
nelsonputra <nelson.putra.id@gmail.com>
2026-02-26 21:11:56 +0700

    Revisi LatihanValidasi – Pertemuan 7
    Memperbaiki kemampuan kode untuk memeriksa 3 input sekaligus

--- Pertemuan 7/LatihanValidasi.java
+++ Pertemuan 7/LatihanValidasi.java
@@ -9,21 +9,22 @@ package pertemuan7;
 
 public class LatihanValidasi {
     public static void main(String[] args){
-        //Inisialisasi nilai awal
-        int nilai = 85;
+        //Inisialisasi nilai awal, menggunakan array karena terdapat "input" lebih dari satu
+        int[]nilai = {-10, 150, 85};
+        //variabel count digunakan sebagai index array agar kita bisa menggunakan elemen yang sesuai tiap iterasi
+        int count = 0;
         System.out.println("=== VALIDASI NILAI UJIAN ===");
         
         //Proses yang akan dilakukan paling tidak satu kali,
         //Pengkondisian agar nilai tidak lebih rendah dari 0 dan lebih dari 100
-        do { 
-            System.out.println("Percobaan input: " + nilai);
-            if(nilai > 100 || nilai < 0){
+        do {
+            System.out.println("Percobaan input: " + nilai[count]);
+            if(nilai[count] > 100 || nilai[count] < 0){
                 System.out.println("Error: Nilai harus antara 0-100!");
-                break;
             } else {
-                System.out.println("Nilai valid: " + nilai);
-                break;
+                System.out.println("Nilai valid: " + nilai[count]);
             }
-        } while (nilai > 0 && nilai < 101);
+            count++;
+        } while (count < 3);
     }
 }
