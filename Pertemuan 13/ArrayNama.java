754a858721a4ea62094aece283394419ccb838b0
nelsonputra <nelson.putra.id@gmail.com>
2026-03-15 15:00:19 +0700

    Latihan - Pertemuan 13
    Latihan penggunaan array 1 dimensi bertipe integer dan string

---
+++ Pertemuan 13/ArrayNama.java
@@ -0,0 +1,14 @@
+package pertemuan13;
+
+public class ArrayNama {
+    public static void main(String[] args) {
+        //Array satu dimensi yang berisi nama dengan tipe String
+        String[] nama = {"Andi", "Budi", "Citra", "Deni", "Eka"};
+        System.out.println("=== DAFTAR NAMA ===");
+        //Perulangan For sebanyak ukuran array
+        for(int index = 0; index < nama.length; index++){
+            //index + 1 diperlukan karena urutan array itu dari 0, jadi agar output lebih sesuai perlu +1
+            System.out.println((index + 1) + ". " + nama[index]);
+        }
+    }
+}
---
+++ Pertemuan 13/ArrayNilai.java
@@ -0,0 +1,17 @@
+package pertemuan13;
+
+public class ArrayNilai {
+    public static void main(String[] args) {
+        //Array satu dimensi yang menyimpan nilai bertipe integer
+        int[] nilai = {85, 90, 78, 92, 88};
+        System.out.println("=== DAFTAR NILAI ===");
+        //For Loop sebanyak isi array lalu menampilkan index sesuai iterasinya
+        //Dibutuhkan index + 1 agar outputnya tidak dari 0 lagi
+        for(int index = 0; index < nilai.length; index++){
+            System.out.println("Nilai " + (index + 1) + ": " + nilai[index]);
+        }
+        //Menampilkan ukuran array
+        System.out.println("");
+        System.out.println("Total Nilai: " + nilai.length);
+    }
+}
